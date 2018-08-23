f8c629688c8a6fd9d770867d865525cd15d74dcd
ltoddy <taoliu14@acm.org>
2018-08-23 19:16:51 +0800

    base64 encryption algorithm encode function

---
+++ tests/test_crypto.rs
@@ -0,0 +1,19 @@
+extern crate parallel;
+
+#[cfg(test)]
+mod test_crypto {
+    use parallel::crypto::*;
+
+    #[test]
+    pub fn test_base64_decode() {
+        assert_eq!(base64_encode(String::from("9153728a-074f-4510-966f-f3098ddb1bd9")),
+                   String::from("OTE1MzcyOGEtMDc0Zi00NTEwLTk2NmYtZjMwOThkZGIxYmQ5"));
+
+        assert_eq!(base64_encode(String::from("2c8d7b62-8dcf-4dfe-a6a2-efe65264a53bx")),
+                   String::from("MmM4ZDdiNjItOGRjZi00ZGZlLWE2YTItZWZlNjUyNjRhNTNieA=="));
+
+        assert_eq!(base64_encode(String::from("78b57816-41c5-4b9e-8ca4-32f41626f9ac")),
+                   String::from("NzhiNTc4MTYtNDFjNS00YjllLThjYTQtMzJmNDE2MjZmOWFj"));
+    }
+
+}
